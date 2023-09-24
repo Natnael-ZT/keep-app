9ad7b1b1bc446aca9788b12892129e8f12dc3c2d
Nati Zerai <natizerai11@gmail.com>
2023-09-24 00:48:55 -0400

    modified css

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import "./Footer.css";
 
 function Footer() {
   const currentYear = new Date().getFullYear();
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import "./Header.css";
 
 function Header() {
   return (
